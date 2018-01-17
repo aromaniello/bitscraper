009ff87b18f8e87d72079e874a0554b60e698a59
Agustin Romaniello <aromaniello9@gmail.com>
2018-01-17 06:42:48 -0300

    modify views and helpers

--- app/helpers/bitcointalk_users_helper.rb
+++ app/helpers/bitcointalk_users_helper.rb
@@ -1,12 +1,29 @@
 module BitcointalkUsersHelper
 
-	def shorten_twitter_link(link)
-		link_to link.gsub("https://twitter.com/",''), link if link
+	def user_to_react(bitcointalk_user)
+		{
+			id: bitcointalk_user.id,
+			username: bitcointalk_user.username,
+			user_url: bitcointalk_user_path(bitcointalk_user),
+			twitter_user_url: bitcointalk_user.twitter_user_url,
+			status_count: bitcointalk_user.status_count,
+			most_recent_status: bitcointalk_user.most_recent_status ? bitcointalk_user.most_recent_status.twitter_url : nil
+		}
 	end
 
-	def most_recent_status(bitcointalk_user)
-		if status = bitcointalk_user.most_recent_status
-			link_to status.twitter_url.gsub("https://twitter.com/",''), status.twitter_url
+	def users_to_react(bitcointalk_users)
+		bitcointalk_users.map { |bitcointalk_user| user_to_react(bitcointalk_user) }
+	end
+
+	def statuses_to_react(twitter_statuses)
+		twitter_statuses.map do |twitter_status|
+			{
+				id: twitter_status.id,
+				week: twitter_status.week,
+				twitter_url: twitter_status.twitter_url,
+				post_date: twitter_status.post_date.strftime("%b %e, %Y %k:%M:%S"),
+				created_at: twitter_status.created_at.strftime("%b %e, %Y %k:%M:%S")
+			}
 		end
 	end
 
